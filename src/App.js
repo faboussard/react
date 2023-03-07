0d440c8f299da76a8e8cc77a14f953545984d4eb
Fanny BOUSSARD <boussard.fanny@gmail.com>
2023-03-07 17:53:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,100 +1,40 @@
-import { React, useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import './App.css';
+import {React, useState} from "react";
+import Player from './Components/Player'
+import Parti from './Components/Parti'
+import './global.css'
 
-const App = () => {
-    const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
-
-
+function App() {
+    const [role, setRole] = useState('défensif')
+    const [players, setPlayers] = useState(
+    [
+        {name: "Fanny", role: "attaquant", img:"https://www.pexels.com/fr-fr/photo/photographie-en-gros-plan-de-kitty-1022158/",
+    },
+        {name:"Emir", role:"défensif", img:"https://www.pexels.com/fr-fr/photo/photographie-en-gros-plan-de-kitty-1022158/",
+    },
+        {name:"Karim", role:"mixte", img:"https://www.pexels.com/fr-fr/photo/photographie-en-gros-plan-de-kitty-1022158/",
+    },
+    ]
+    const showPlayers = true;
     return (
-             <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#000000",
-                    },
-                },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    collisions: {
-                        enable: true,
-                    },
-                    move: {
-                        directions: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 6,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
-                },
-                detectRetina: true,
-            }}
-        />
-    );
-};
+        <div className="App">
+            {showPlayers ? (
+                <>
+                <input type='text' 
+                onChange={() => {
+                    console.log(e.target.value);
+                    setRole(e.target.value);
+                }}
+                />
+                <div className="flex flex-wrap justify-center">
+                   {players.map((player) => {
+                    }}}
+                </div>
+                </>
+            ) : (
+                <p>You cannot see the players</p>
+            )}
+        </div> 
+    )
+}
+
 export default App;
